86649f0b1b58d23f54bf1f90c117fd91eb0e6e07
Aporve <aporvep@gmail.com>
2021-04-15 19:19:18 +0530

    Testing Event Handling

--- custom.js
+++ custom.js
@@ -1,17 +1,18 @@
 
 window.addEventListener('message', function(eventData) {
-    let clickedButton = btnID;
     try { 
-         if(clickedButton){
-           
+         if(app.data){
+           app.log(app.data);
          }
         if (JSON.parse(eventData.data)) {
             let event = JSON.parse(eventData.data);
              if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" || event.data.code == "clickedButton") {
+                app.log('Event Successfull');
                 app.triggerIntent('vemo-2021-talking-points-asset-class-return-outlooks')
             }
             else if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" ||  event.data.code == "clickedButton"){
-                run_recaptcha();
+                app.log('Event Successfull');
+                app.triggerIntent('vemo-2021-talking-points-asset-class-return-outlooks')
                 return;
             } 
             else{
