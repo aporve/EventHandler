587407ebf641ba7ee8df96ff5edbe2615d15c4e9
Aporve <aporvep@gmail.com>
2021-04-16 11:41:58 +0530

    Cleaning Up code

--- custom.js
+++ custom.js
@@ -3,25 +3,26 @@ window.addEventListener('message', function(eventData) {
     console.log(eventData);
     try { 
         console.log('Parsing data')
-        console.log(JSON.parse(eventData.data));
+        // console.log(JSON.parse(eventData.data));
         let ymEvent = JSON.parse(eventData.data)
-        console.log(JSON.parse(ymEvent.data), "Event Data")
-       console.log((JSON.parse(ymEvent.data)).event.code)
+        // console.log(JSON.parse(ymEvent.data), "Event Data")
+    //    console.log((JSON.parse(ymEvent.data)).event.code)
         let data = JSON.parse(ymEvent.data);
-        console.log(data.event.code);
-        console.log(data.event.data);
+        // console.log(data.event.code);
+        // console.log(data.event.data);
        if(((JSON.parse(ymEvent.data)).event.code) == 'clickedButton') {
             console.log('Get Data');
+            console.log(data.event.data);
        }
        
         if (JSON.parse(eventData.data)) {
             console.log('Parse---->')
             let event = JSON.parse(eventData.data);
-            console.log("event specs")
-            console.log(event)
-            console.log(typeof event)
-            console.log(event.event_code)
-            console.log(event.data, "Event Data ---->")
+            // console.log("event specs")
+            // console.log(event)
+            // console.log(typeof event)
+            // console.log(event.event_code)
+            // console.log(event.data, "Event Data ---->")
             switch (event.event_code){
                 case "ym-client-event":
                     console.log("event successful")
