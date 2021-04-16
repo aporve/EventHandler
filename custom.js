4b78b181da87f6b442629d2186759c0a28a038df
Aporve <aporvep@gmail.com>
2021-04-16 10:09:10 +0530

    Updated Code with Event Data

--- custom.js
+++ custom.js
@@ -7,11 +7,14 @@ window.addEventListener('message', function(eventData) {
         if (JSON.parse(eventData.data)) {
             console.log('Parse---->')
             let event = JSON.parse(eventData.data);
+            let data = event.JSON.parse(event.data);
             console.log("event specs")
             console.log(event)
             console.log(typeof event)
             console.log(event.event_code)
             console.log(event.data, "Event Data ---->")
+            console.log(JSON.parse(event.data), "Event Data ---->")
+            console.error(data.event.code)
             switch (event.event_code){
                 case "ym-client-event":
                     console.log("event successful")
