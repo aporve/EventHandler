1b47ba52992fb1b8b664f63516e0d77b4e50a20f
Aporve <aporvep@gmail.com>
2021-04-16 08:59:06 +0530

    Code changes

--- custom.js
+++ custom.js
@@ -1,13 +1,12 @@
 
-window.addEventListener('click', function(eventData) {
-    console.error('Custom Function from Platform ------>>>>')
-    app.log(eventData);
+window.addEventListener('message', function(eventData) {
+    console.log('onload---->')
     console.log(eventData);
     try { 
-         if(app.data){
-           app.log(app.data);
-         }
+        console.log('Parsing data')
+        console.log(JSON.parse(eventData.data));
         if (JSON.parse(eventData.data)) {
+            console.log('Parse---->')
             let event = JSON.parse(eventData.data);
              if (event.event_code == "custom-event" || event.data || event.data.code == "ym-client-event" || event.data.code == "clickedButton") {
                 app.log('Event Successfull');
