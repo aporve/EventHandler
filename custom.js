fd165fbd77d53083ac031f8b063b8ebb8a0fa76e
aporve <aporvep@gmail.com>
2021-06-07 18:48:43 +0530

    Revert to Normal with new Icons

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@
         var iFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];         
         var modularBars = document.createElement('script');
         modularBars.type = 'text/javascript';
-        modularBars.src = 'https://ghost-tt.github.io/ym-frames/addFrames.js';
+        modularBars.src = 'https://aporve.github.io/EventHandler/addFrame.js';
         iFrameHead.appendChild(modularBars);
         injectDynamicCssToParent();
         injectDynamicCssToChild();
