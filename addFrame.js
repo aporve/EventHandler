26d5f2ee1569446339a1b713e6979b9e1a0bd00f
aporve <aporvep@gmail.com>
2021-06-07 23:02:30 +0530

    Event handler 4

--- addFrame.js
+++ addFrame.js
@@ -5,42 +5,42 @@
         `<div>
             <div style="position: absolute; top: 0; left: 0px; right: 0; bottom: 0;">
                 <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: column; justify-content: center; background-color: #fff; padding: 0; width: 3rem; list-style: none; height: calc(100% - 55px); justify-content: space-between;border: 1px solid #ccc">
-                    <a to="/" id="e1" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 1.2rem; height: auto;"/>
+                    <a to="/" id="e1" onclick="sendEvent('VEMO')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                    <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png" style="width: 1.2rem; height: auto;"/>
+                    <a to="/" id="e1" href="#" onclick="sendEvent('VEMO')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/GnxBWRVeIm6i1622800692538.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                    <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/4dnzmSDugrwW1622800597860.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                    <a to="/" id="e2" href="#" onclick="sendEvent('ACTIVE')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/4dnzmSDugrwW1622800597860.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                    <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/peOgkPdE8n0A1622800752931.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                    <a to="/" id="e3" href="#" onclick="sendEvent('ASSET')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/peOgkPdE8n0A1622800752931.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                    <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/FPloPRu3LmfA1622800812350.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                    <a to="/" id="e4" href="#" onclick="sendEvent('ECON')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/FPloPRu3LmfA1622800812350.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                    <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/sULHcUPQH5RL1622800858305.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                    <a to="/" id="e5" href="#" onclick="sendEvent('INVESTOR')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/sULHcUPQH5RL1622800858305.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                  <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/VLla29GmwVpi1622800914321.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                  <a to="/" id="e6" href="#" onclick="sendEvent('POPFAQ')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/VLla29GmwVpi1622800914321.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                  <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/o2BBDya1HiQT1622800956549.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                  <a to="/" id="e7" href="#" onclick="sendEvent('RETIREMENT')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/o2BBDya1HiQT1622800956549.png" style="width: 1.2rem; height: auto;" />
                     </a>
-                  <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
-                        <img src="https://cdn.yellowmessenger.com/g6Fdrr5NeUj01622800983140.png" style="width: 1.2rem; height: auto; z-index:-1" />
+                  <a to="/" id="e8" href="#" onclick="sendEvent('VCMM')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                        <img src="https://cdn.yellowmessenger.com/g6Fdrr5NeUj01622800983140.png" style="width: 1.2rem; height: auto;" />
                     </a>
                 </ul>
             </div>
             <div style="position: fixed; top: 0; right: 0; width: calc(100% - 3rem - 2px); z-index: 1;">
                 <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: row; align-items: center; justify-content: flex-end; background-color: #fff; padding: 0; list-style: none; height: 3rem;border: 1px solid #ccc;border-left: 0">
                     <div>
-                        <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
+                        <a to="/" href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
                             <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 1.2rem; height: auto;" />
                         </a>
-                        <a href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
+                        <a to="/" href="#" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer;">
                             <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 1.2rem; height: auto;" />
                         </a>
                     </div>
@@ -53,26 +53,42 @@
     }
   })();
 
-
-// function sendEvent(btnID) {
-//     console.log('In Evemt Function')
-//     console.log(document.getElementById('ymIframe'));
-//     // parent.postMessage("ACTIVE", "*")
-//     document.getElementById('ymIframe').contentWindow.postMessage({
-//         event_code: 'ym-client-event',
-//         data: {
-//             event: {
-//              code: 'ACTIVE',
-//              data: ""
-//             }
-//         }
-//    }, '*');
-//     return;
-// }
-
-
-document.getElementById('e1').onclick = function () {
+  document.getElementById('e1').onclick = function () {
     // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
-    parent.postMessage("ACTIVE", "*")
+    parent.postMessage("myevent", "*")
 };
 
+document.getElementById('e2').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e3').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e4').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e5').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e6').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e7').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
+
+document.getElementById('e8').onclick = function () {
+    // parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+    parent.postMessage("myevent", "*")
+};
--- custom.js
+++ custom.js
@@ -46,162 +46,116 @@ window.addEventListener('message', function(eventData) {
     console.log(eventData);
     try{
 
-        console.error( 'Data----------------->>>',eventData.data)
+        console.error( 'Data----------------->>>',eventData.data);
+        if(eventData.data == 'VEMO') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "VEMO",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+
+        if(eventData.data == 'ACTIVE') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "ACTIVE",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+        if(eventData.data == 'ASSET') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "ASSET",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+        if(eventData.data == 'ECON') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "ECON",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+        if(eventData.data == 'INVESTOR') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "INVESTOR",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+
+        if(eventData.data == 'POPFAQ') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "POPFAQ",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+        if(eventData.data == 'RETIREMENT') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "RETIREMENT",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+
+        if(eventData.data == 'VCMM') {
+            document.getElementById('ymIframe').contentWindow.postMessage({
+                event_code: 'ym-client-event',
+                data: {
+                    event: {
+                     code: "VCMM",
+                     data: ""
+                    }
+                }
+           }, '*');
+           return;
+        }
+        
     }catch(error){
         console.log(error);
         return;
     }
-    // try { 
-    //     console.log('Parsing data')
-    //     // console.log(JSON.parse(eventData.data));
-    //     let ymEvent = JSON.parse(eventData.data)
-    //     // console.log(JSON.parse(ymEvent.data), "Event Data")
-    //     //    console.log((JSON.parse(ymEvent.data)).event.code)
-    //     let data = JSON.parse(ymEvent.data);
-    //     console.log(data.event.code, "Data------>");
-    //     // console.log(data.event.data);
-    //    if(((JSON.parse(ymEvent.data)).event.code) == 'VEMO') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "VEMO",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //        return;
-    //    }
-
-
-    //    if(((JSON.parse(ymEvent.data)).event.code) == 'ACTIVE') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "ACTIVE",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'ASSET') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "ASSET",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'ECON') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "ECON",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'INVESTOR') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "INVESTOR",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'POPFAQ') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "POPFAQ",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'RETIREMENT') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "RETIREMENT",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-
-    //     if(((JSON.parse(ymEvent.data)).event.code) == 'VCMM') {
-    //         console.log('Get Data');
-    //         console.log(data.event.data);
-    //         document.getElementById('ymIframe').contentWindow.postMessage({
-    //             event_code: 'ym-client-event',
-    //             data: {
-    //                 event: {
-    //                  code: "VCMM",
-    //                  data: ""
-    //                 }
-    //             }
-    //        }, '*');
-    //         return;
-    //     }
-       
-    //     if (JSON.parse(eventData.data)) {
-    //         console.log('Parse---->')
-    //         let event = JSON.parse(eventData.data);
-    //         // console.log("event specs")
-    //         // console.log(event)
-    //         // console.log(typeof event)
-    //         // console.log(event.event_code)
-    //         // console.log(event.data, "Event Data ---->")
-    //         switch (event.event_code){
-    //             case "ym-client-event":
-    //                 console.log("event successful")
-    //                 break;
-    //             default:
-    //                 console.log("event unsuccessful")
-    //                 break;
-    //         }
-    //         console.log("ending up here")
-    //         return;
-    //      }
-    // } catch (error) {
-    //     return;
-    // }
+    
 }, false);
