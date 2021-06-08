1c0cc917de43cd718451a50a5632b1483fc02ece
aporve <aporvep@gmail.com>
2021-06-08 10:47:20 +0530

    Dynamic FAQ Name

--- addFrame.js
+++ addFrame.js
@@ -6,7 +6,7 @@
         `<div>
         <div style="position: absolute; top: 0; left: 0px; right: 0; bottom: 0;">
             <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: column; justify-content: center; background-color: #fff; padding: 0; width: 3rem; list-style: none; height: calc(100% - 55px); justify-content: space-between;border: 1px solid #ccc">
-                <li id="e1" onclick="sendEvent('VEMO')" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
+                <li class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
                     <img src="https://cdn.yellowmessenger.com/LS9XYE9urVcj1605084428877.png" style="width: 1.2rem; height: auto;" />
                 </li>
                 <li id="e1" class="sc-jSFjdj jcTaHb" style="text-decoration: none; color: #fff; width: 100%; padding: 1rem; cursor: pointer; display: flex;">
@@ -38,7 +38,7 @@
         <div style="position: fixed; top: 0; right: 0; width: calc(100% - 3rem - 2px); z-index: 1;">
             <ul class="sc-eCApnc jvVggF" style="display: flex; flex-direction: row; align-items: center; justify-content: flex-end; background-color: #fff; padding: 0; list-style: none; height: 3rem;border: 1px solid #ccc;border-left: 0">
                 <div style="justify-content: flex-start; width: 100%; padding: 0 0 0 20px;">
-                    <p>Vanguard</p>
+                    <p id="faq">Vanguard</p>
                 </div>
                 <div style="display: flex;">
                    <!-- <ul> -->
@@ -60,33 +60,41 @@
   })();
 
   document.getElementById('e1').onclick = function () {
-    parent.postMessage("ACTIVE", "*")
+    document.getElementById('faq').innerHTML='VEMO'
+    parent.postMessage("VEMO", "*")
 };
 
 document.getElementById('e2').onclick = function () {
+    document.getElementById('faq').innerHTML='ACTIVE'
     parent.postMessage("ACTIVE", "*")
 };
 
 document.getElementById('e3').onclick = function () {
+    document.getElementById('faq').innerHTML='ASSET'
     parent.postMessage("ASSET", "*")
 };
 
 document.getElementById('e4').onclick = function () {
+    document.getElementById('faq').innerHTML='ECON'
     parent.postMessage("ECON", "*")
 };
 
 document.getElementById('e5').onclick = function () {
+    document.getElementById('faq').innerHTML='INVESTOR'
     parent.postMessage("INVESTOR", "*")
 };
 
 document.getElementById('e6').onclick = function () {
+    // document.getElementById('faq').innerHTML='POPFAQ'
     parent.postMessage("POPFAQ", "*")
 };
 
 document.getElementById('e7').onclick = function () {
+    document.getElementById('faq').innerHTML='ASSET'
     parent.postMessage("RETIREMENT", "*")
 };
 
 document.getElementById('e8').onclick = function () {
+    document.getElementById('faq').innerHTML='ASSET'
     parent.postMessage("VCMM", "*")
 };
